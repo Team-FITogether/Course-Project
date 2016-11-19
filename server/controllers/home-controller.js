269646a60c8b5f8ca77cab0fc4784a19ead0e6fd
Sparx92 <stefankoserkov@gmail.com>
2016-11-19 20:09:02 +0200

    Added user menu, cosmetic changes

--- server/controllers/home-controller.js
+++ server/controllers/home-controller.js
@@ -8,9 +8,11 @@ const userValidator = require("../utils/user-validator");
 
 function loadHomePage(req, res) {
     let pathToReadFrom = path.join(config.rootPath, "server/views/home/home.pug");
+    let username = req.user ? req.user.username : "";
+    let isLoggedIn = !!req.user;
     let isAdmin = !req.user ? false : userValidator.isInRole(req.user, "admin");
     let compiledFile = pug.compileFile(pathToReadFrom);
-    let html = compiledFile({ isAdmin });
+    let html = compiledFile({ isAdmin, isLoggedIn, username });
 
     res.send(html);
 }
