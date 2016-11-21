8c2fe6c012a17d2e31b7c6eac034e9cf9e3fc17f
TeodSt <teod_stoyanova@abv.bg>
2016-11-21 23:57:07 +0200

    Implemented Search by exercise.

--- server/controllers/searches-controller.js
+++ server/controllers/searches-controller.js
@@ -4,7 +4,7 @@ function findUsers(username, isLoggedIn, res) {
     mongoose
         .model("user")
         .where({ username })
-        .select('_id username')
+        .select("_id username")
         .exec((err, data) => {
             if (err) {
                 console.log(err);
@@ -19,13 +19,36 @@ function findUsers(username, isLoggedIn, res) {
         });
 }
 
+function findExercises(exerciseName, isLoggedIn, res) {
+
+    mongoose
+        .model("exercise")
+        .where({ name: new RegExp(exerciseName, "i") })
+        .select("_id name")
+        .exec((err, data) => {
+            if (err) {
+                console.log(err);
+            } else {
+                res.render("searches/found-exercises.pug", {
+                    exercises: data,
+                    viewBag: {
+                        isLoggedIn
+                    }
+                });
+            }
+        });
+}
+
 function findEntities(req, res) {
     let query = req.query;
     let entityName = query.entityName;
     let isLoggedIn = !!req.user;
+    console.log(entityName);
 
     if (entityName === "users") {
         findUsers(query.searchValue, isLoggedIn, res);
+    } else if (entityName === "exercises") {
+        findExercises(query.searchValue, isLoggedIn, res);
     }
 }
 
--- server/routers/exercises-router.js
+++ server/routers/exercises-router.js
@@ -6,5 +6,6 @@ module.exports = (app) => {
     app.get("/exercises", controllers.exercises.getAllCategoriesOfExercise);
     app.get("/exercises/explanations", controllers.exercises.getSingleExercise);
     app.get("/exercises/single-exercise", controllers.exercises.getAllExercisesByCategory);
+
     app.post("/excercises/comments/add", controllers.exercises.addComment);
 };
