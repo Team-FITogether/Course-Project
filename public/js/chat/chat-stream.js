d37af0af3a3b3c9307cd7fa64a3f1163dd4f5cdb
gabriel.belchev <belchev.gb@gmail.com>
2016-11-25 12:51:18 -0800

    The chat invitation now does not force both browsers to redirect to the chat room, but sends an invitation. The invitations doesn't have styles. If the receiver is offline, error message is shown.

--- public/js/chat/chat-stream.js
+++ public/js/chat/chat-stream.js
@@ -1,4 +1,4 @@
-/* globals EventSource window */
+/* globals EventSource window $ */
 
 (function () {
     "use strict";
@@ -11,7 +11,16 @@
             let chatRoomUrl = parsedData.chatRoomUrl;
 
             if (chatRoomUrl) {
-                window.location.href = chatRoomUrl;
+                let $invitationWindow = $("<div />");
+                let $heading = $("<h3 />");
+                let $chatRoomLink = $("<a />");
+
+                $heading.text(`${parsedData.senderUsername} ви изпрати покана за чат.`);
+                $chatRoomLink.text("Приеми");
+                $chatRoomLink.attr("href", chatRoomUrl);
+                $invitationWindow.append($heading);
+                $invitationWindow.append($chatRoomLink);
+                $("body").append($invitationWindow);
             }
         }, false);
 
--- server/controllers/chat-controller.js
+++ server/controllers/chat-controller.js
@@ -1,6 +1,8 @@
 "use strict";
 
 const encryption = require("../utils/encryption");
+const mongoose = require("mongoose");
+const User = mongoose.model("user");
 
 let connections = new Map();
 let rooms = new Set();
@@ -37,10 +39,25 @@ function handleInvitation(req, res) {
     let receiver = connections.get(receiverUsername);
     let roomName = getRandomRoomName();
     let chatRoomUrl = `/chat-room/${roomName}`;
-    let data = JSON.stringify({ chatRoomUrl });
+    let data = JSON.stringify({ chatRoomUrl, senderUsername: req.user.username });
 
-    receiver.sseSend(data);
-    res.redirect(chatRoomUrl);
+    if (!receiver) {
+        let user = req.user;
+        user.isAdmin = req.user.roles.indexOf("admin") !== -1;
+        User
+            .find({ username: req.query.receiver })
+            .then(foundUsers => {
+                res.render("user/found-user-profile", {
+                    foundUser: foundUsers[0],
+                    user,
+                    isOffline: true
+                });
+            })
+            .catch(console.log);
+    } else {
+        receiver.sseSend(data);
+        res.redirect(chatRoomUrl);
+    }
 }
 
 function loadChatRoom(req, res) {
