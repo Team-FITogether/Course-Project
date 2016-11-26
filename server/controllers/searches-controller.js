14e152940dab93790df39d38280f97d5cfd9c6bc
pspassova <petya.spassova@abv.bg>
2016-11-26 22:39:37 +0200

    Implemented searching for foods and recipes

--- server/controllers/searches-controller.js
+++ server/controllers/searches-controller.js
@@ -46,16 +46,67 @@ function findExercises(exerciseName, isLoggedIn, req, res) {
         });
 }
 
+function findFoods(foodName, isLoggedIn, req, res) {
+    let user = req.user;
+    if (req.user) {
+        user.isAdmin = req.user.roles.indexOf("admin") !== -1;
+    }
+
+    mongoose
+        .model("fooddetails")
+        .where({ title: new RegExp(foodName, "i") })
+        .select("_id title")
+        .exec((err, data) => {
+            if (err) {
+                console.log(err);
+            } else {
+                res.render("searches/found-foods.pug", {
+                    fooddetails: data,
+                    viewBag: {
+                        isLoggedIn
+                    }
+                });
+            }
+        });
+}
+
+function findRecipes(recipeName, isLoggedIn, req, res) {
+    let user = req.user;
+    if (req.user) {
+        user.isAdmin = req.user.roles.indexOf("admin") !== -1;
+    }
+
+    mongoose
+        .model("recipe")
+        .where({ title: new RegExp(recipeName, "i") })
+        .select("_id title")
+        .exec((err, data) => {
+            if (err) {
+                console.log(err);
+            } else {
+                res.render("searches/found-recipes.pug", {
+                    recipes: data,
+                    viewBag: {
+                        isLoggedIn
+                    }
+                });
+            }
+        });
+}
+
 function findEntities(req, res) {
     let query = req.query;
     let entityName = query.entityName;
     let isLoggedIn = !!req.user;
-    console.log(entityName);
 
     if (entityName === "users") {
         findUsers(query.searchValue, isLoggedIn, req, res);
     } else if (entityName === "exercises") {
         findExercises(query.searchValue, isLoggedIn, req, res);
+    } else if (entityName === "foods") {
+        findFoods(query.searchValue, isLoggedIn, req, res);
+    } else if (entityName === "recipes") {
+        findRecipes(query.searchValue, isLoggedIn, req, res);
     }
 }
 
