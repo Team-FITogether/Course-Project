212d927326283b468144b39801c1a0161c4d6b1a
gabriel.belchev <belchev.gb@gmail.com>
2016-11-25 16:10:53 -0800

    Chat scroll is added and the chat is always scrolled to the bottom.

--- public/js/chat/chat.js
+++ public/js/chat/chat.js
@@ -1,74 +1,89 @@
 /* globals io window $ */
 
-let socket = io.connect();
-let pathName = window.location.pathname;
-let lastIndexOfForwardSlash = pathName.lastIndexOf("/");
-let roomName = pathName.substr(lastIndexOfForwardSlash + 1);
+(function() {
+    "use strict";
 
-function attachSubmitFormEvent(username, avatarSrc) {
-    $("form").on("submit", () => {
-        let message = $("#tb-message").val();
-        let data = {
-            sender: username,
-            room: roomName,
-            message,
-            avatarSrc
-        };
+    let socket = io.connect();
+    let pathName = window.location.pathname;
+    let lastIndexOfForwardSlash = pathName.lastIndexOf("/");
+    let roomName = pathName.substr(lastIndexOfForwardSlash + 1);
 
-        socket.emit("chat message to room", data);
-        $("#tb-message").val("");
-        return false;
-    });
-}
+    function attachSubmitFormEvent(username, avatarSrc) {
+        $("form").on("submit", () => {
+            let message = $("#tb-message").val();
+            let data = {
+                sender: username,
+                room: roomName,
+                message,
+                avatarSrc
+            };
 
-function attachChatMessageEvent($li, $message, $messageContainer, $avatar, username, $allMessages) {
-    socket.on("chat message", data => {
-        let $currentLi = $li.clone(true);
-        let $currentMessage = $message.clone();
-        let $currentAvatar = $avatar.clone();
-        let $currentMessageContainer = $messageContainer.clone();
+            socket.emit("chat message to room", data);
+            $("#tb-message").val("");
+            return false;
+        });
+    }
 
-        if (data.sender === username) {
-            $currentMessageContainer.addClass("right");
-        } else {
-            $currentMessageContainer.addClass("left");
+    function attachChatMessageEvent($li, $message, $messageContainer, $avatar, username, $allMessages) {
+        socket.on("chat message", data => {
+            let $currentLi = $li.clone(true);
+            let $currentMessage = $message.clone();
+            let $currentAvatar = $avatar.clone();
+            let $currentMessageContainer = $messageContainer.clone();
+
+            if (data.sender === username) {
+                $currentMessageContainer.addClass("right");
+            } else {
+                $currentMessageContainer.addClass("left");
+            }
+
+            $currentAvatar.attr("src", data.avatarSrc);
+            $currentMessageContainer.append($currentAvatar);
+            $currentMessage.text(data.message);
+            $currentMessageContainer.append($currentMessage);
+            $currentLi.append($currentMessageContainer);
+            $allMessages.append($currentLi);
+        });
+    }
+
+    function attachFullRoomEvent() {
+        socket.on("full room", () => {
+            $("body")
+                .empty()
+                .text("ROOM IS FULL");
+        });
+    }
+
+    let isScrolled = false;
+
+    function updateScroll() {
+        let allMessages = $("#all-messages-container")[0];
+        if (!isScrolled) {
+            allMessages.scrollTop = allMessages.scrollHeight;
         }
+    }
 
-        $currentAvatar.attr("src", data.avatarSrc);
-        $currentMessageContainer.append($currentAvatar);
-        $currentMessage.text(data.message);
-        $currentMessageContainer.append($currentMessage);
-        $currentLi.append($currentMessageContainer);
-        $allMessages.append($currentLi);
+    socket.on("connect", () => {
+        socket.emit("room", roomName);
+
+        const username = $("#username-holder").attr("username");
+        const avatarSrc = $("#avatarSrc-holder").attr("avatar-src");
+
+        attachSubmitFormEvent(username, avatarSrc);
+        let $li = $("<li />");
+        let $avatar = $("<img />");
+        let $message = $("<span />");
+        let $allMessages = $("#messages");
+        let $messageContainer = $("<div />");
+
+        $allMessages.attr("id", "all-messages-container");
+        $avatar.addClass("img img-responsive img-circle chat-avatar");
+        $message.addClass("chat-message");
+        $messageContainer.append($avatar);
+        $messageContainer.addClass("message-container");
+
+        setInterval(updateScroll, 1000);
+        attachChatMessageEvent($li, $message, $messageContainer, $avatar, username, $allMessages)
+        attachFullRoomEvent();
     });
-}
-
-function attachFullRoomEvent() {
-    socket.on("full room", () => {
-        $("body")
-            .empty()
-            .text("ROOM IS FULL");
-    });
-}
-
-socket.on("connect", () => {
-    socket.emit("room", roomName);
-
-    const username = $("#username-holder").attr("username");
-    const avatarSrc = $("#avatarSrc-holder").attr("avatar-src");
-
-    attachSubmitFormEvent(username, avatarSrc);
-
-    let $li = $("<li />");
-    let $avatar = $("<img />");
-    let $message = $("<span />");
-    let $allMessages = $("#messages");
-    let $messageContainer = $("<div />");
-    $avatar.addClass("img img-responsive img-circle chat-avatar");
-    $message.addClass("chat-message");
-    $messageContainer.append($avatar);
-    $messageContainer.addClass("message-container");
-
-    attachChatMessageEvent($li, $message, $messageContainer, $avatar, username, $allMessages)
-    attachFullRoomEvent();
-});
+} ());
