9794a8731c4738e18958b5765e09624de35ad7b7
Sparx92 <stefankoserkov@gmail.com>
2016-11-28 16:02:09 +0200

    Actually set mongoose connection string to production.

--- server/configurations/database.js
+++ server/configurations/database.js
@@ -5,6 +5,6 @@ const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 
 module.exports = config => {
-    mongoose.connect(config.db.local);
+    mongoose.connect(config.connectionString.prod);
     require("../models/user").addAdminUser();
 };
--- server/configurations/index.js
+++ server/configurations/index.js
@@ -8,9 +8,10 @@ const path = require("path");
 const rootPath = path.join(__dirname, "/../../");
 
 module.exports = {
-    db: {
-        local: "mongodb://localhost:27017/FiTogether",
-        remote: `mongodb://${remoteDbUsername}:${remoteDbPassword}@ds050539.mlab.com:50539/fitogether`},
+    connectionString: {
+        dev: "mongodb://localhost:27017/FiTogether",
+        prod: `mongodb://${remoteDbUsername}:${remoteDbPassword}@ds050539.mlab.com:50539/fitogether`
+    },
     port: process.env.PORT || 8080,
     rootPath
 };
