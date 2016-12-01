e611f2957c8c95305a207441c16749134986f7cc
gabriel <belchev.gb@gmail.com>
2016-12-01 13:30:08 +0200

    loadCreateArticle() of articles-controller is tested

--- tests/controllers/articles-controller-tests.js
+++ tests/controllers/articles-controller-tests.js
@@ -19,30 +19,34 @@ describe("loadCreateArticlePage tests", () => {
 
         controller.loadCreateArticlePage(req, res);
         sinon.assert.calledOnce(commonSpy);
-        commonSpy.restore();
     });
 
     it("common.setIsAdminUser() should be called with req and userValidator objects", () => {
         let commonMock = { setIsAdminUser() { } };
-        let commonSpy = sinon.spy(commonMock, "setIsAdminUser");
         let dataMock = {};
         let req = {};
         let res = { render() { } };
-
         let userValidatorMock = {};
+
+        let commonSpy = sinon.spy(commonMock, "setIsAdminUser");
         let controller = articlesController({ common: commonMock, userValidator: userValidatorMock, data: dataMock });
 
         controller.loadCreateArticlePage(req, res);
         sinon.assert.calledWith(commonSpy, req, userValidatorMock);
-        commonSpy.restore();
     });
 
     it("res.render() should be called once and with CREATE_ARTICLE_VIEW string and { user } object", () => {
         let commonMock = { setIsAdminUser() { } };
-        let commonSpy = sinon.spy(commonMock, "setIsAdminUser");
         let dataMock = {};
-        let req = {};
+        let req = { user: {} };
         let res = { render() { } };
-        
+        let userValidatorMock = {};
+        let CREATE_ARTICLE_VIEW = "articles/create-article";
+
+        let resSpy = sinon.spy(res, "render");
+        let controller = articlesController({ common: commonMock, userValidator: userValidatorMock, data: dataMock });
+
+        controller.loadCreateArticlePage(req, res);
+        sinon.assert.calledWith(resSpy, CREATE_ARTICLE_VIEW, { user: req.user });
     });
 });
