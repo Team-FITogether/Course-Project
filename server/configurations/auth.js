b6ae57316510bac9efab942eac7dd6b14f7fae34
pspassova <petya.spassova@abv.bg>
2016-12-03 00:02:01 +0200

    OAuth changes, so it can work with the cloud

--- server/configurations/auth.js
+++ server/configurations/auth.js
@@ -3,13 +3,13 @@ module.exports = {
     "facebookAuth": {
         "clientID": "1029874553801352", 
         "clientSecret": "fe3b79d5484f09baff47037fefe110a4", 
-        "callbackURL": "http://localhost:8080/auth/facebook/callback",
+        "callbackURL": "https://fitogether.herokuapp.com/auth/facebook/callback",
         "usernameSuffix": " - Facebook"
     },
     "googleAuth": {
         "clientID": "917226922419-fa7hflm8vd2u9st6sgbjn9mc1g008otr.apps.googleusercontent.com", 
         "clientSecret": "JK1R_PznQSeJE35P-ImnGZP6", 
-        "callbackURL": "http://localhost:8080/auth/google/callback/",
+        "callbackURL": "https://fitogether.herokuapp.com/auth/google/callback/",
         "usernameSuffix": " - Google"
     }
 };
