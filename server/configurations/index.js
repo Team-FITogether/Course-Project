ec9d507f96cfa16aae2723fcf0a4e8d73d8fd06d
Sparx92 <stefankoserkov@gmail.com>
2016-12-02 17:10:51 +0200

    Atempting to add enviroment variables for heroku depolyment

--- server/configurations/index.js
+++ server/configurations/index.js
@@ -1,17 +1,17 @@
 "use strict";
 
-const remoteDbUsername = "fitogetheruser";
-const remoteDbPassword = "telerikacademy";
-
 const path = require("path");
 
 const rootPath = path.join(__dirname, "/../../");
 
+let connectionStrings = {
+    production: process.env.CONNECTION_STRING,
+    development: "mongodb://localhost:27017/fitogether"
+};
+
 module.exports = {
-    connectionString: {
-        dev: "mongodb://localhost:27017/fitogether",
-        prod: `mongodb://${remoteDbUsername}:${remoteDbPassword}@ds050539.mlab.com:50539/fitogether`
-    },
+    environment: process.env.NODE_ENV || "development",
+    connectionString: connectionStrings[process.env.NODE_ENV || "development"],
     port: process.env.PORT || 8080,
     rootPath
 };
