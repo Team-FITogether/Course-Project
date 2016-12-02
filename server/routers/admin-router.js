8ad4b03e7ea3b6f379f64558b3d28fb233c70256
pspassova <petya.spassova@abv.bg>
2016-12-02 13:58:45 +0200

    Extracted admin-router

---
+++ server/routers/admin-router.js
@@ -0,0 +1,15 @@
+"use strict"
+
+module.exports = ({ app, userValidator, controllers }) => {
+    const adminController = controllers.admin;
+
+    app.get("/admin-panel", userValidator.isAdminUserMiddleware, userValidator.isUserLoggedIn, adminController.loadAdminPanel);
+    app.get("/select-category", userValidator.isAdminUserMiddleware, userValidator.isUserLoggedIn, adminController.addNewFood);
+
+    app.post("/users/set-role", userValidator.isAdminUserMiddleware, userValidator.isUserLoggedIn, adminController.addRole);
+    app.post("/exercises/add-new", userValidator.isAdminUserMiddleware, userValidator.isUserLoggedIn, adminController.addNewExerciseCategory);
+    app.post("/foods/add-new", userValidator.isAdminUserMiddleware, userValidator.isUserLoggedIn, adminController.addNewFoodCategory);
+    app.post("/recipes/add-new", userValidator.isAdminUserMiddleware, userValidator.isUserLoggedIn, adminController.addNewRecipe);
+    app.post("/diets/add-new", userValidator.isAdminUserMiddleware, userValidator.isUserLoggedIn, adminController.addNewDiet);
+    app.post("/fooddetails/add-new", userValidator.isAdminUserMiddleware, userValidator.isUserLoggedIn, adminController.addNewFood);
+}
