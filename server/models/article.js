fb3ed0b5d76856252aaec526f5480288e26e1751
Sparx92 <stefankoserkov@gmail.com>
2016-12-05 01:36:04 +0200

    Fixed likes for articles

--- server/models/article.js
+++ server/models/article.js
@@ -7,19 +7,19 @@ const validationConstants = require("../utils/validation-constants");
 let articleSchema = new Schema({
     mainHeader: { type: String, require: true, minlength: validationConstants.MIN_LENGTH, maxlength: validationConstants.MAX_TEXT_LENGTH },
     subHeader: { type: String, minlength: validationConstants.MIN_LENGTH, maxlength: validationConstants.MAX_TEXT_LENGTH },
-    author: { type: String, require: true, minlength: validationConstants.MIN_LENGTH, maxlength: validationConstants.MAX_USER_NAME_LENGTH },
-    imgSrc: { type: String, minlength: validationConstants.MIN_LENGTH, maxlength: validationConstants.MAX_TEXT_LENGTH },
-    genre: { type: String, require: true, minlength: validationConstants.MIN_LENGTH, maxlength: validationConstants.MAX_TEXT_LENGTH },
-    body: { type: String, require: true, minlength: validationConstants.MIN_LENGTH, maxlength: validationConstants.MAX_TEXT_LENGTH },
-    likes: { type: Number, default: validationConstants.MIN_NUMBER, min: validationConstants.MIN_NUMBER },
+    author: { type: String, require: true },
+    imgSrc: { type: String },
+    genre: { type: String, require: true },
+    body: { type: String, require: true },
+    likes: { type: Number, default: 0 },
     deletedOn: { type: Date, default: null },
     createdOn: { type: Date, default: Date.now },
     usersLiked: [{
-        user: { type: String, minlength: validationConstants.MIN_LENGTH, maxlength: validationConstants.MAX_USER_NAME_LENGTH }
+        user: String
     }],
     comments: [{
-        content: { type: String, minlength: validationConstants.MIN_LENGTH },
-        author: { type: String, minlength: validationConstants.MIN_LENGTH, maxlength: validationConstants.MAX_USER_NAME_LENGTH },
+        content: String,
+        author: String,
         postDate: Date
     }]
 });
